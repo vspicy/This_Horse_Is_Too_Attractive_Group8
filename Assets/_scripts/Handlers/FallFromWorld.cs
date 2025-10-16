97fd7ff103fd4386fb11d29160e9164538e22c99
Hebrjebr <iggy090605@gmail.com>
2025-10-16 16:38:41 -0700

    changes to tutorial level
    works with player controller

--- Assets/_scripts/Handlers/FallFromWorld.cs
+++ Assets/_scripts/Handlers/FallFromWorld.cs
@@ -18,7 +18,7 @@ public class FallFromWorld : MonoBehaviour
     {
         if (transform.position.y < killFloor)
         {
-            transform.position = new Vector3(0f, 1.5f, 0f);
+            transform.position = new Vector3(0f, 4f, 0f);
         }
     }
 }
