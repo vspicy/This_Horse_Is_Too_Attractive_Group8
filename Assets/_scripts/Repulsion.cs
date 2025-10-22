7345d5e5e7c06e3978e9514415b9b1cd4cfd97e5
Kaylie Ruffner <kruffner0408@gmail.com>
2025-10-22 16:21:08 -0700

    Repulsion 01

--- Assets/_scripts/Repulsion.cs
+++ Assets/_scripts/Repulsion.cs
@@ -14,12 +14,25 @@ public class Repulsion : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // call when want to trigger the repulsion
+    public void PlayerRepulsion(Vector3 sourcePosition)
+    {
+        // calculate direction away from the source 
+        Vector3 direction = (transform.position - sourcePosition).normalized;
+
+        //clear any downward velocity if a clean launch is wanted
+        rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+
+        //apply the impluse force
+        rb.AddForce(direction * repulsionForce, ForceMode.Impulse);
     }
 }
