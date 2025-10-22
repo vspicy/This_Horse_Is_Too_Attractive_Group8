3e08588b77834d069d67e49832dae3db03d956a1
Kaylie Ruffner <kruffner0408@gmail.com>
2025-10-22 16:03:14 -0700

    Creation of the repulsion script

---
+++ Assets/_scripts/Repulsion.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Author: [Ruffner, Kaylie]
+ * Creation Date: [10-22-2025]
+ * Summarization: [This script handles the repulsion for the player, where the player can repel themselves.]
+ */
+public class Repulsion : MonoBehaviour
+{
+    private float repulsionForce = 10f;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
