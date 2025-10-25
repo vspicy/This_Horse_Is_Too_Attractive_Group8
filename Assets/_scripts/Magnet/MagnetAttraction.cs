30d59b637ffa4cb61d3c73d77a46cd5acdae3d1d
Hebrjebr <iggy090605@gmail.com>
2025-10-25 08:18:13 -0700

    well okay then
    a

---
+++ Assets/_scripts/Magnet/MagnetAttraction.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Name: Ian Phurchpean
+ * Date: 24 October 2025
+ * Objective: Code that allows the player to attract themselves to other objects with a magnet (but Updated).
+ */
+
+public class MagnetAttraction : MonoBehaviour
+{
+    public GameObject magnetRadius;
+
+    // Update every frame
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("LMB Down: Magnet Active");
+            magnetRadius.SetActive(true);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("LMB Up: Magnet Inactive");
+            magnetRadius.SetActive(false);
+        }
+    }
+}
