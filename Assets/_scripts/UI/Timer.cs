6d778977dcd42b3de96ba4adfd3b36d1bde624b4
Hebrjebr <iggy090605@gmail.com>
2025-11-11 10:46:36 -0800

    a timer
    timing

--- Assets/_scripts/UI/Timer.cs
+++ Assets/_scripts/UI/Timer.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-/*
+
+/*
  * Name: Ian Phurchpean
  * Last Updated: 9 November 2025
  * In-game UI Manager to represent the player's time.
@@ -11,13 +12,32 @@ using TMPro;
 public class Timer : MonoBehaviour
 {
     // Initialize Variables
+    [Header("References")]
     public TMP_Text time; // time
+    public bool playing;
     float givenTime;
+
+    public float finalTime;
+
+    [Header("Time")]
+    int minutes;
+    int seconds;
+    int milliseconds;
 
     // Update is called once per frame
     void Update()
     {
         Stopwatch();
+        finalTime = givenTime;
+
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            minutes = 0;
+            seconds = 0;
+            milliseconds = 0;
+        }
+
     }
 
     /// <summary>
@@ -25,11 +45,18 @@ public class Timer : MonoBehaviour
     /// </summary>
     public void Stopwatch()
     {
-        // Keep track of the time
-        givenTime += Time.deltaTime;
-        int minutes = Mathf.FloorToInt(givenTime / 60); // find minute (Convert float to int)
-        int seconds = Mathf.FloorToInt(givenTime % 60); // find second (Convert float to int)
+        if (playing == true)
+        {
+            // Keep track of the time
+            givenTime += Time.deltaTime;
+            minutes = Mathf.FloorToInt(givenTime / 60); // find minute (Convert float to int)
+            seconds = Mathf.FloorToInt(givenTime % 60); // find second (Convert float to int)
+            milliseconds = Mathf.FloorToInt((givenTime * 100) % 100); // find millisecond (Convert float to int)
 
-        time.text = "TIME: " + string.Format("{0:00}:{1:00}", minutes, seconds); // display current time
+            time.text = "Time: " + minutes.ToString("00") + ":" + seconds.ToString("00") + "." + milliseconds.ToString("00"); // Display time on the screen
+        }
+
     }
 }
+
+
