80e727472d96f1fca2c2f45808627594756eaaed
BoseHayden <HAYDEN.BOSE@GMAIL.COM>
2025-12-05 23:12:37 -0800

    Implemented a working cannon

---
+++ Assets/_scripts/Cannon.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    public Transform firePoint;
+    public Transform cannonCenter;
+    public GameObject cannonBallPrefab;
+    private Vector3 cannonBallVelocity;
+    private float cannonFireRate = 2;
+    private float cannonBallStrength = 16;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("CannonShoot", 0, cannonFireRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CannonShoot()
+    {
+        GameObject CannonBall = Instantiate(cannonBallPrefab, firePoint.position, Quaternion.identity);
+
+        cannonBallVelocity = cannonCenter.position - firePoint.position;
+
+        Rigidbody rb = CannonBall.GetComponent<Rigidbody> ();
+
+        rb.AddForce(cannonBallVelocity * cannonBallStrength, ForceMode.Impulse);
+    }
+}
