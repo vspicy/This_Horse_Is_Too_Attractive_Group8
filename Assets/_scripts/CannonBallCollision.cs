a72c651ad8cd8d9b93c9e3fec2388c7186b6ba4d
BoseHayden <HAYDEN.BOSE@GMAIL.COM>
2025-12-09 11:25:27 -0800

    polished the cannon ball a bit so that the repulsion transform script disables upon contact as to prevent clipping through the cannon ball while moving

---
+++ Assets/_scripts/CannonBallCollision.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBallCollision : MonoBehaviour
+{
+    public RepulsionTransform repulsionTransform;
+    public GameObject cannonBallPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("CannonBall"))
+        {
+            print("Player has hit cannon ball");
+            MovementManagement();
+        }
+    }
+
+    private void MovementManagement()
+    {
+        repulsionTransform.enabled = false;
+        Invoke("TurnOnMovement", 1);
+    }
+
+    private void TurnOnMovement()
+    {
+        repulsionTransform.enabled = true;
+    }
+}
